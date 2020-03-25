28b998464836e9d45c853658ab3ba0f9e2909d43
Corinna Fabre <corinna.fabre@gmail.com>
2020-03-25 09:24:58 -0700

    Passes tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,11 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+
+  intersection = []
+
+  list1.each do |num|
+    intersection << num if list2.include?(num) == true
+  end
+  return intersection
 end
+
+intersection([2, 3, 4], [4, 5, 6])
