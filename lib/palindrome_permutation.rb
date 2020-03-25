2c721a74634697681b4fede8ee2140c34e00fe5c
Corinna Fabre <corinna.fabre@gmail.com>
2020-03-25 14:56:01 -0700

    Passes palindrome_permutation tests.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,22 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+
+  return true if string == ""
+
+  hash = string.split("").each_with_object(Hash.new(0)) do |char, hash|
+    hash[char] += 1
+  end
+  
+  not_palindrome = 0
+
+  hash.each do |key, value|
+    if hash[key] != 2
+      not_palindrome += 1
+    end
+  end
+
+  return not_palindrome == 1
+
 end
+
+
